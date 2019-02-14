2edd047a413ece6dc2599b404e49d372997e88a2
John Michael Brown <jmichaelbrown132737@gmail.com>
2019-02-14 09:27:03 -0800

    add image and profile assets

---
+++ src/assets/god-array.js
@@ -0,0 +1,12 @@
+Import odinImage from './Odin_der_Goettervater.jpg';
+
+const NorseGods = [
+  {
+    name: "Odin",
+    photo: odinImage,
+    about: "In Germanic mythology, Odin (/ˈoʊdɪn/; from Old Norse: Óðinn, IPA: [ˈoːðinː]) is a widely revered god. In Norse mythology, from which stems most surviving information about the god, Odin is associated with wisdom, healing, death, royalty, the gallows, knowledge, battle, sorcery, poetry, frenzy, and the runic alphabet, and is the husband of the goddess Frigg. In wider Germanic mythology and paganism, Odin was known in Old English as Wōden, in Old Saxon as Wōdan, and in Old High German as Wuotan or Wōtan, all stemming from the reconstructed Proto-Germanic theonym *wōđanaz.",
+    quote: "I was given to Odin, myself to myself. (Odin speaking about himself)"
+  },
+]
+
+export { NorseGods }
