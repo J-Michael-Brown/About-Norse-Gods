6fc02c1f7ddcf95ad7ef7613000c1be74773a4f4
John Michael Brown <jmichaelbrown132737@gmail.com>
2019-02-14 10:01:39 -0800

    add Tyr, add quote author property

--- src/assets/god-array.js
+++ src/assets/god-array.js
@@ -1,12 +1,20 @@
-Import odinImage from './Odin_der_Goettervater.jpg';
+import odinImage from './Odin_der_Goettervater.jpg';
+import tyrImage from './Tyr_der_Schwertgott.jpg';
 
 const NorseGods = [
   {
-    name: "Odin",
+    name: 'Odin',
     photo: odinImage,
     about: "In Germanic mythology, Odin (/ˈoʊdɪn/; from Old Norse: Óðinn, IPA: [ˈoːðinː]) is a widely revered god. In Norse mythology, from which stems most surviving information about the god, Odin is associated with wisdom, healing, death, royalty, the gallows, knowledge, battle, sorcery, poetry, frenzy, and the runic alphabet, and is the husband of the goddess Frigg. In wider Germanic mythology and paganism, Odin was known in Old English as Wōden, in Old Saxon as Wōdan, and in Old High German as Wuotan or Wōtan, all stemming from the reconstructed Proto-Germanic theonym *wōđanaz.",
-    quote: "I was given to Odin, myself to myself. (Odin speaking about himself)"
+    quote: 'I was given to Odin, myself to myself.'
   },
-]
+  {
+    name: 'Tyr',
+    photo: tyrImage,
+    about: 'Norse war god, but also the god who, more than any other, presides over matters of law and justice. His role in the surviving Viking Age myths is relatively slight, and his status in the later part of the Viking Age may have been correspondingly minor. But this wasn’t always the case. Other kinds of evidence show us that Tyr was once one of the most important gods to the Norse and other Germanic peoples.',
+    quote: "It should be noted that, from the Germanic point of view, there is no contradiction between the concepts ‘god of War’ and ‘god of Law.’",
+    quoteAuthor: 'Jan de Vries'
+  }
+];
 
-export { NorseGods }
+export default NorseGods;
